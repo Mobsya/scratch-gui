5bee63d83682d108b56d283b112fbdf6f3d9d6b6
Corentin Jabot <corentinjabot@gmail.com>
2018-09-13 08:54:48 +0200

    Build for 6a39ca681402bd28bfa7211f02c8dbe59fd9c84f

--- gui.js
+++ gui.js
@@ -1,132 +1,36 @@
 var GUI =
-webpackJsonpGUI([0],{
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["gui"],{
 
-/***/ 159:
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css":
+/*!*****************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--5-1!./node_modules/postcss-loader/src??postcss!./src/playground/index.css ***!
+  \*****************************************************************************************************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
 
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+// module
+exports.push([module.i, "html,\nbody,\n.index_app_3Qs6X {\n    /* probably unecessary, transitional until layout is refactored */\n    width: 100%; \n    height: 100%;\n    margin: 0;\n\n    /* Setting min height/width makes the UI scroll below those sizes */\n    min-width: 1024px;\n    min-height: 640px; /* Min height to fit sprite/backdrop button */\n}\n\n/* @todo: move globally? Safe / side FX, for blocks particularly? */\n\n* { -webkit-box-sizing: border-box; box-sizing: border-box; }\n", ""]);
 
-__webpack_require__(160);
-
-var _react = __webpack_require__(0);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = __webpack_require__(12);
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _reactModal = __webpack_require__(16);
-
-var _reactModal2 = _interopRequireDefault(_reactModal);
-
-var _analytics = __webpack_require__(11);
-
-var _analytics2 = _interopRequireDefault(_analytics);
-
-var _gui = __webpack_require__(25);
-
-var _gui2 = _interopRequireDefault(_gui);
-
-var _index = __webpack_require__(573);
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-if (false) {
-    // Warn before navigating away
-    window.onbeforeunload = function () {
-        return true;
-    };
-}
-
-// Register "base" page view
-_analytics2.default.pageview('/');
-
-var appTarget = document.createElement('div');
-appTarget.className = _index2.default.app;
-document.body.appendChild(appTarget);
-
-_reactModal2.default.setAppElement(appTarget);
-
-_reactDom2.default.render(_react2.default.createElement(_gui2.default, null), appTarget);
-
-/***/ }),
-
-/***/ 160:
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-__webpack_require__(161).polyfill();
-
-
-/***/ }),
-
-/***/ 161:
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-/**
- * Code refactored from Mozilla Developer Network:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
- */
-
-
-
-function assign(target, firstSource) {
-  if (target === undefined || target === null) {
-    throw new TypeError('Cannot convert first argument to object');
-  }
-
-  var to = Object(target);
-  for (var i = 1; i < arguments.length; i++) {
-    var nextSource = arguments[i];
-    if (nextSource === undefined || nextSource === null) {
-      continue;
-    }
-
-    var keysArray = Object.keys(Object(nextSource));
-    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-      var nextKey = keysArray[nextIndex];
-      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-      if (desc !== undefined && desc.enumerable) {
-        to[nextKey] = nextSource[nextKey];
-      }
-    }
-  }
-  return to;
-}
-
-function polyfill() {
-  if (!Object.assign) {
-    Object.defineProperty(Object, 'assign', {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: assign
-    });
-  }
-}
-
-module.exports = {
-  assign: assign,
-  polyfill: polyfill
+// exports
+exports.locals = {
+	"app": "index_app_3Qs6X"
 };
 
-
 /***/ }),
 
-/***/ 573:
+/***/ "./src/playground/index.css":
+/*!**********************************!*\
+  !*** ./src/playground/index.css ***!
+  \**********************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(574);
+var content = __webpack_require__(/*! !../../node_modules/css-loader??ref--5-1!../../node_modules/postcss-loader/src??postcss!./index.css */ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css");
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -140,55 +44,158 @@ var options = {"hmr":true}
 options.transform = transform
 options.insertInto = undefined;
 
-var update = __webpack_require__(3)(content, options);
+var update = __webpack_require__(/*! ../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
 
 if(content.locals) module.exports = content.locals;
 
-if(false) {
-	module.hot.accept("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./index.css", function() {
-		var newContent = require("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./index.css");
+if(false) {}
 
-		if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+/***/ }),
 
-		var locals = (function(a, b) {
-			var key, idx = 0;
+/***/ "./src/playground/index.jsx":
+/*!**********************************!*\
+  !*** ./src/playground/index.jsx ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-			for(key in a) {
-				if(!b || a[key] !== b[key]) return false;
-				idx++;
-			}
+"use strict";
 
-			for(key in b) idx--;
 
-			return idx === 0;
-		}(content.locals, newContent.locals));
+__webpack_require__(/*! es6-object-assign/auto */ "./node_modules/es6-object-assign/auto.js");
 
-		if(!locals) throw new Error('Aborting CSS HMR due to changed css-modules locals.');
+__webpack_require__(/*! core-js/fn/array/includes */ "./node_modules/core-js/fn/array/includes.js");
 
-		update(newContent);
-	});
+__webpack_require__(/*! intl */ "./node_modules/intl/index.js");
 
-	module.hot.dispose(function() { update(); });
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _analytics = __webpack_require__(/*! ../lib/analytics */ "./src/lib/analytics.js");
+
+var _analytics2 = _interopRequireDefault(_analytics);
+
+var _appStateHoc = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+
+var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
+
+var _browserModal = __webpack_require__(/*! ../components/browser-modal/browser-modal.jsx */ "./src/components/browser-modal/browser-modal.jsx");
+
+var _browserModal2 = _interopRequireDefault(_browserModal);
+
+var _supportedBrowser = __webpack_require__(/*! ../lib/supported-browser */ "./src/lib/supported-browser.js");
+
+var _supportedBrowser2 = _interopRequireDefault(_supportedBrowser);
+
+var _index = __webpack_require__(/*! ./index.css */ "./src/playground/index.css");
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Register "base" page view
+// Polyfills
+_analytics2.default.pageview('/'); // For Safari 9
+
+var appTarget = document.createElement('div');
+appTarget.className = _index2.default.app;
+document.body.appendChild(appTarget);
+
+if ((0, _supportedBrowser2.default)()) {
+    // require needed here to avoid importing unsupported browser-crashing code
+    // at the top level
+    __webpack_require__(/*! ./render-gui.jsx */ "./src/playground/render-gui.jsx").default(appTarget);
+} else {
+    _browserModal2.default.setAppElement(appTarget);
+    var WrappedBrowserModalComponent = (0, _appStateHoc2.default)(_browserModal2.default, true /* localesOnly */);
+    var handleBack = function handleBack() {};
+    // eslint-disable-next-line react/jsx-no-bind
+    _reactDom2.default.render(_react2.default.createElement(WrappedBrowserModalComponent, { onBack: handleBack }), appTarget);
 }
 
 /***/ }),
 
-/***/ 574:
+/***/ "./src/playground/render-gui.jsx":
+/*!***************************************!*\
+  !*** ./src/playground/render-gui.jsx ***!
+  \***************************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(2)(false);
-// imports
+"use strict";
 
 
-// module
-exports.push([module.i, "html,\nbody,\n.index_app_3Qs6X {\n    /* probably unecessary, transitional until layout is refactored */\n    width: 100%; \n    height: 100%;\n    margin: 0;\n}\n\n/* @todo: move globally? Safe / side FX, for blocks particularly? */\n\n* { -webkit-box-sizing: border-box; box-sizing: border-box; }\n", ""]);
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
-// exports
-exports.locals = {
-	"app": "index_app_3Qs6X"
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _appStateHoc = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+
+var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
+
+var _gui = __webpack_require__(/*! ../containers/gui.jsx */ "./src/containers/gui.jsx");
+
+var _gui2 = _interopRequireDefault(_gui);
+
+var _hashParserHoc = __webpack_require__(/*! ../lib/hash-parser-hoc.jsx */ "./src/lib/hash-parser-hoc.jsx");
+
+var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
+
+var _titledHoc = __webpack_require__(/*! ../lib/titled-hoc.jsx */ "./src/lib/titled-hoc.jsx");
+
+var _titledHoc2 = _interopRequireDefault(_titledHoc);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/*
+ * Render the GUI playground. This is a separate function because importing anything
+ * that instantiates the VM causes unsupported browsers to crash
+ * {object} appTarget - the DOM element to render to
+ */
+exports.default = function (appTarget) {
+    _gui2.default.setAppElement(appTarget);
+    var WrappedGui = (0, _hashParserHoc2.default)((0, _appStateHoc2.default)((0, _titledHoc2.default)(_gui2.default)));
+
+    // TODO a hack for testing the backpack, allow backpack host to be set by url param
+    var backpackHostMatches = window.location.href.match(/[?&]backpack_host=([^&]*)&?/);
+    var backpackHost = backpackHostMatches ? backpackHostMatches[1] : null;
+
+    var backpackOptions = {
+        visible: true,
+        host: backpackHost
+    };
+    if (false) {}
+
+    _reactDom2.default.render(_react2.default.createElement(WrappedGui, { backpackOptions: backpackOptions }), appTarget);
 };
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************!*\
+  !*** ./locale-data/complete.js (ignored) ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
 
 /***/ })
 
-},[159]);
+},[["./src/playground/index.jsx","lib.min"]]]);
 //# sourceMappingURL=gui.js.map
--- player.js
+++ player.js
@@ -1,66 +1,37 @@
 var GUI =
-webpackJsonpGUI([1],{
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["player"],{
 
-/***/ 579:
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/player.css":
+/*!******************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--5-1!./node_modules/postcss-loader/src??postcss!./src/playground/player.css ***!
+  \******************************************************************************************************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
 
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+// module
+exports.push([module.i, ".player_stage-only_3WHZN {\n    width: calc(480px + 1rem);\n}\n\n.player_stage-only_3WHZN * {\n    -webkit-box-sizing: border-box;\n            box-sizing: border-box;\n}\n", ""]);
 
-var _react = __webpack_require__(0);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = __webpack_require__(12);
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _box = __webpack_require__(7);
-
-var _box2 = _interopRequireDefault(_box);
-
-var _gui = __webpack_require__(25);
-
-var _gui2 = _interopRequireDefault(_gui);
-
-var _player = __webpack_require__(580);
-
-var _player2 = _interopRequireDefault(_player);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-if (false) {
-    // Warn before navigating away
-    window.onbeforeunload = function () {
-        return true;
-    };
-}
-
-var Player = function Player() {
-    return _react2.default.createElement(
-        _box2.default,
-        { className: _player2.default.stageOnly },
-        _react2.default.createElement(_gui2.default, {
-            isPlayerOnly: true,
-            isFullScreen: false
-        })
-    );
+// exports
+exports.locals = {
+	"stage-only": "player_stage-only_3WHZN",
+	"stageOnly": "player_stage-only_3WHZN"
 };
-
-var appTarget = document.createElement('div');
-document.body.appendChild(appTarget);
-
-_reactDom2.default.render(_react2.default.createElement(Player, null), appTarget);
 
 /***/ }),
 
-/***/ 580:
+/***/ "./src/playground/player.css":
+/*!***********************************!*\
+  !*** ./src/playground/player.css ***!
+  \***********************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(581);
+var content = __webpack_require__(/*! !../../node_modules/css-loader??ref--5-1!../../node_modules/postcss-loader/src??postcss!./player.css */ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/player.css");
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -74,56 +45,127 @@ var options = {"hmr":true}
 options.transform = transform
 options.insertInto = undefined;
 
-var update = __webpack_require__(3)(content, options);
+var update = __webpack_require__(/*! ../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
 
 if(content.locals) module.exports = content.locals;
 
-if(false) {
-	module.hot.accept("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./player.css", function() {
-		var newContent = require("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./player.css");
-
-		if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-
-		var locals = (function(a, b) {
-			var key, idx = 0;
-
-			for(key in a) {
-				if(!b || a[key] !== b[key]) return false;
-				idx++;
-			}
-
-			for(key in b) idx--;
-
-			return idx === 0;
-		}(content.locals, newContent.locals));
-
-		if(!locals) throw new Error('Aborting CSS HMR due to changed css-modules locals.');
-
-		update(newContent);
-	});
-
-	module.hot.dispose(function() { update(); });
-}
+if(false) {}
 
 /***/ }),
 
-/***/ 581:
+/***/ "./src/playground/player.jsx":
+/*!***********************************!*\
+  !*** ./src/playground/player.jsx ***!
+  \***********************************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(2)(false);
-// imports
+"use strict";
 
 
-// module
-exports.push([module.i, ".player_stage-only_3WHZN {\n    width: calc(480px + 1rem);\n}\n\n.player_stage-only_3WHZN * {\n    -webkit-box-sizing: border-box;\n            box-sizing: border-box;\n}\n", ""]);
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-// exports
-exports.locals = {
-	"stage-only": "player_stage-only_3WHZN",
-	"stageOnly": "player_stage-only_3WHZN"
+var _classnames = __webpack_require__(/*! classnames */ "./node_modules/classnames/index.js");
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+var _propTypes = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _reactRedux = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+
+var _box = __webpack_require__(/*! ../components/box/box.jsx */ "./src/components/box/box.jsx");
+
+var _box2 = _interopRequireDefault(_box);
+
+var _gui = __webpack_require__(/*! ../containers/gui.jsx */ "./src/containers/gui.jsx");
+
+var _gui2 = _interopRequireDefault(_gui);
+
+var _hashParserHoc = __webpack_require__(/*! ../lib/hash-parser-hoc.jsx */ "./src/lib/hash-parser-hoc.jsx");
+
+var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
+
+var _appStateHoc = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+
+var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
+
+var _titledHoc = __webpack_require__(/*! ../lib/titled-hoc.jsx */ "./src/lib/titled-hoc.jsx");
+
+var _titledHoc2 = _interopRequireDefault(_titledHoc);
+
+var _mode = __webpack_require__(/*! ../reducers/mode */ "./src/reducers/mode.js");
+
+var _player = __webpack_require__(/*! ./player.css */ "./src/playground/player.css");
+
+var _player2 = _interopRequireDefault(_player);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+if (false) {}
+
+var Player = function Player(_ref) {
+    var isPlayerOnly = _ref.isPlayerOnly,
+        onSeeInside = _ref.onSeeInside,
+        projectId = _ref.projectId;
+    return _react2.default.createElement(
+        _box2.default,
+        {
+            className: (0, _classnames2.default)(_defineProperty({}, _player2.default.stageOnly, isPlayerOnly))
+        },
+        isPlayerOnly && _react2.default.createElement(
+            'button',
+            { onClick: onSeeInside },
+            'See inside'
+        ),
+        _react2.default.createElement(_gui2.default, {
+            enableCommunity: true,
+            isPlayerOnly: isPlayerOnly,
+            projectId: projectId
+        })
+    );
 };
+
+Player.propTypes = {
+    isPlayerOnly: _propTypes2.default.bool,
+    onSeeInside: _propTypes2.default.func,
+    projectId: _propTypes2.default.string
+};
+
+var mapStateToProps = function mapStateToProps(state) {
+    return {
+        isPlayerOnly: state.scratchGui.mode.isPlayerOnly
+    };
+};
+
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+    return {
+        onSeeInside: function onSeeInside() {
+            return dispatch((0, _mode.setPlayer)(false));
+        }
+    };
+};
+
+var ConnectedPlayer = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(Player);
+var WrappedPlayer = (0, _hashParserHoc2.default)((0, _appStateHoc2.default)((0, _titledHoc2.default)(ConnectedPlayer)));
+
+var appTarget = document.createElement('div');
+document.body.appendChild(appTarget);
+
+_reactDom2.default.render(_react2.default.createElement(WrappedPlayer, { isPlayerOnly: true }), appTarget);
 
 /***/ })
 
-},[579]);
+},[["./src/playground/player.jsx","lib.min"]]]);
 //# sourceMappingURL=player.js.map
